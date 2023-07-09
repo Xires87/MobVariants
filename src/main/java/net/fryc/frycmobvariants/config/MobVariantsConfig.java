aff966a700cd06de138c7506ee648cf8ea567212
Xires87 <frycmod@gmail.com>
2023-07-09 14:22:08 +0200

    changed config serializer + new config options

--- src/main/java/net/fryc/frycmobvariants/config/MobVariantsConfig.java
+++ src/main/java/net/fryc/frycmobvariants/config/MobVariantsConfig.java
@@ -3,52 +3,87 @@ package net.fryc.frycmobvariants.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "frycmobvariants")
 public class MobVariantsConfig implements ConfigData {
 
     //cave
+    @Comment("Zombies have a chance to convert into stronger variant when they spawn below this Y level")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("cave")
     @ConfigEntry.BoundedDiscrete(max = 300, min = -64)
     public int zombieToForgottenConvertLevelY = 26;
 
+    @Comment("When this value is below 0, chance of converting increases by 1% with every Y level below value specified in previous option")
+    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Category("cave")
+    @ConfigEntry.BoundedDiscrete(max = 100, min = -1)
+    public int fixedChanceToConvertZombieUnderSelectedYLevel = -1;
+
+    @Comment("Skeletons have a chance to convert into stronger variant when they spawn below this Y level")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("cave")
     @ConfigEntry.BoundedDiscrete(max = 300, min = -64)
     public int skeletonToUndeadWarriorConvertLevelY = 26;
 
+    @Comment("When this value is below 0, chance of converting increases by 1% with every Y level below value specified in previous option")
+    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Category("cave")
+    @ConfigEntry.BoundedDiscrete(max = 100, min = -1)
+    public int fixedChanceToConvertSkeletonUnderSelectedYLevel = -1;
+
+    @Comment("Spiders have a chance to convert into stronger variant when they spawn below this Y level")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("cave")
     @ConfigEntry.BoundedDiscrete(max = 300, min = -64)
-    public int spiderToBlackSpiderConvertLevelY = 26;
+    public int spiderToArmoredSpiderConvertLevelY = 26;
 
+    @Comment("When this value is below 0, chance of converting increases by 1% with every Y level below value specified in previous option")
+    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Category("cave")
+    @ConfigEntry.BoundedDiscrete(max = 100, min = -1)
+    public int fixedChanceToConvertSpiderUnderSelectedYLevel = -1;
+
+    @Comment("Creepers have a chance to convert into stronger variant when they spawn below this Y level")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("cave")
     @ConfigEntry.BoundedDiscrete(max = 300, min = -64)
     public int creeperToCaveCreeperConvertLevelY = 26;
 
+    @Comment("When this value is below 0, chance of converting increases by 1% with every Y level below value specified in previous option")
+    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Category("cave")
+    @ConfigEntry.BoundedDiscrete(max = 100, min = -1)
+    public int fixedChanceToConvertCreeperUnderSelectedYLevel = -1;
+
+
     //nether
+    @Comment("For example, when set to 10, Wither Skeleton will have 10% chance to convert to Executioner after spawning")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("nether")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int witherSkeletonConvertChance = 11;
 
+    @Comment("For example, when set to 10, Ghast will have 10% chance to convert to Nightmare after spawning")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("nether")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int ghastConvertChance = 26;
 
+    @Comment("For example, when set to 10, Piglin will have 10% chance to convert to Infected Piglin after spawning")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("nether")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int piglinConvertChance = 29;
 
+    @Comment("For example, when set to 10, Piglin Brute will have 10% chance to convert to Infected Piglin Brute after spawning")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("nether")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int piglinBruteConvertChance = 39;
 
+    @Comment("For example, when set to 30, Skeleton will have 30% chance to convert to Soul Stealer after spawning (in nether)")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("nether")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
@@ -56,16 +91,19 @@ public class MobVariantsConfig implements ConfigData {
 
 
     //biomes
+    @Comment("For example, when set to 90, Zombie will have 90% chance to convert to Explorer after spawning in jungle, swamp or lush cave")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("biome")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int zombieToExplorerConvertChance = 75;
 
+    @Comment("For example, when set to 90, Zombie will have 90% chance to convert to Frozen Zombie after spawning in snowy biome")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("biome")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int zombieToFrozenZombieConvertChance = 80;
 
+    @Comment("For example, when set to 70, Spider will have 70% chance to convert to Tropical Spider after spawning in jungle")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("biome")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
@@ -73,20 +111,24 @@ public class MobVariantsConfig implements ConfigData {
 
 
     //mob attributes
+    @Comment("Skeletons with enchanted bow always convert to undead warrior with bow")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("mobattributes")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int undeadWarriorSpawnWithBowChance = 50;
 
+    @Comment("20 = 1 second")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("mobattributes")
     public int undeadWarriorsWeaknessDuration = 300;
 
+    @Comment("Nightmare's chance to shoot single fireball (like normal ghast)")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("mobattributes")
     @ConfigEntry.BoundedDiscrete(max = 100, min = 0)
     public int nightmareShootSingleFireballChance = 50;
 
+    @Comment("Additional magic damage dealt by Soul Stealer. On normal difficulty this value is increased by 1. On hard, by 3")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Category("mobattributes")
     public float soulStealersBaseMagicDamage = 1.0F;

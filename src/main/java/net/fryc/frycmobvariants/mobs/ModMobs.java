ab0e0fc3520af2d979b87bafe6b43b727bf972c3
Xires87 <frycmod@gmail.com>
2024-04-24 16:24:25 +0200

    fixed slime dimensions

--- src/main/java/net/fryc/frycmobvariants/mobs/ModMobs.java
+++ src/main/java/net/fryc/frycmobvariants/mobs/ModMobs.java
@@ -1,7 +1,6 @@
 package net.fryc.frycmobvariants.mobs;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.fryc.frycmobvariants.MobVariants;
 import net.fryc.frycmobvariants.mobs.biome.*;
 import net.fryc.frycmobvariants.mobs.cave.ArmoredSpiderEntity;
@@ -9,7 +8,7 @@ import net.fryc.frycmobvariants.mobs.cave.CaveCreeperEntity;
 import net.fryc.frycmobvariants.mobs.cave.ForgottenEntity;
 import net.fryc.frycmobvariants.mobs.cave.UndeadWarriorEntity;
 import net.fryc.frycmobvariants.mobs.nether.*;
-import net.minecraft.entity.EntityDimensions;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.mob.HostileEntity;
@@ -23,90 +22,90 @@ public class ModMobs {
     public static final EntityType<ForgottenEntity> FORGOTTEN = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "forgotten"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ForgottenEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.95F)).build());
+            EntityType.Builder.create(ForgottenEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<UndeadWarriorEntity> UNDEAD_WARRIOR = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "undead_warrior"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, UndeadWarriorEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.99F)).build());
+            EntityType.Builder.create(UndeadWarriorEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.99F).eyeHeight(1.74F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<ArmoredSpiderEntity> ARMORED_SPIDER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "armored_spider"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ArmoredSpiderEntity::new).dimensions(EntityDimensions.fixed(1.4F, 0.9F)).build());
+            EntityType.Builder.create(ArmoredSpiderEntity::new, SpawnGroup.MONSTER).dimensions(1.4F, 0.9F).eyeHeight(0.65F).passengerAttachments(0.765F).maxTrackingRange(8).build());
 
     public static final EntityType<CaveCreeperEntity> CAVE_CREEPER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "cave_creeper"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CaveCreeperEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.73F)).build());
+            EntityType.Builder.create(CaveCreeperEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.7F).maxTrackingRange(8).build());
 
     //biome variants
     public static final EntityType<ExplorerEntity> EXPLORER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "explorer"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ExplorerEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.95F)).build());
+            EntityType.Builder.create(ExplorerEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<BloatedCorpseEntity> BLOATED_CORPSE = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "bloated_corpse"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, BloatedCorpseEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.95F)).build());
+            EntityType.Builder.create(BloatedCorpseEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<FrozenZombieEntity> FROZEN_ZOMBIE = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "frozen_zombie"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, FrozenZombieEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.95F)).build());
+            EntityType.Builder.create(FrozenZombieEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<TropicalSpiderEntity> TROPICAL_SPIDER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "tropical_spider"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, TropicalSpiderEntity::new).dimensions(EntityDimensions.fixed(1.4F, 0.9F)).build());
+            EntityType.Builder.create(TropicalSpiderEntity::new, SpawnGroup.MONSTER).dimensions(1.4F, 0.9F).eyeHeight(0.65F).passengerAttachments(0.765F).maxTrackingRange(8).build());
 
     public static final EntityType<CorsairEntity> CORSAIR = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "corsair"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CorsairEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.99F)).build());
+            EntityType.Builder.create(CorsairEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.99F).eyeHeight(1.74F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<ToxicSlimeEntity> TOXIC_SLIME = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "toxic_slime"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ToxicSlimeEntity::new).dimensions(EntityDimensions.changing(2.04F, 2.04F)).build());
+            EntityType.Builder.create(ToxicSlimeEntity::new, SpawnGroup.MONSTER).dimensions(0.52F, 0.52F).eyeHeight(0.325F).spawnBoxScale(4.0F).maxTrackingRange(10).build());
 
 
     //nether variants
     public static final EntityType<ExecutionerEntity> EXECUTIONER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "executioner"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ExecutionerEntity::new).dimensions(EntityDimensions.fixed(0.73F, 2.6F)).fireImmune().build());
+            EntityType.Builder.create(ExecutionerEntity::new, SpawnGroup.MONSTER).makeFireImmune().allowSpawningInside(Blocks.WITHER_ROSE).dimensions(0.83F, 2.6F).eyeHeight(2.2F).vehicleAttachment(-0.875F).maxTrackingRange(8).build());
 
     public static final EntityType<NightmareEntity> NIGHTMARE = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "nightmare"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, NightmareEntity::new).dimensions(EntityDimensions.fixed(4.0F, 4.0F)).fireImmune().build());
+            EntityType.Builder.create(NightmareEntity::new, SpawnGroup.MONSTER).makeFireImmune().dimensions(4.0F, 4.0F).eyeHeight(2.6F).passengerAttachments(4.0625F).vehicleAttachment(0.5F).maxTrackingRange(10).build());
 
     public static final EntityType<InfectedPiglinEntity> INFECTED_PIGLIN = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "infected_piglin"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, InfectedPiglinEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.95F)).fireImmune().build());
+            EntityType.Builder.create(InfectedPiglinEntity::new, SpawnGroup.MONSTER).makeFireImmune().dimensions(0.6F, 1.95F).eyeHeight(1.79F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<InfectedPiglinBruteEntity> INFECTED_PIGLIN_BRUTE = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "infected_piglin_brute"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, InfectedPiglinBruteEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.95F)).fireImmune().build());
+            EntityType.Builder.create(InfectedPiglinBruteEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.95F).makeFireImmune().eyeHeight(1.79F).passengerAttachments(2.0125F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<ZombifiedPiglinBruteEntity> ZOMBIFIED_PIGLIN_BRUTE = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "zombified_piglin_brute"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ZombifiedPiglinBruteEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.95F)).fireImmune().build());
+            EntityType.Builder.create(ZombifiedPiglinBruteEntity::new, SpawnGroup.MONSTER).makeFireImmune().dimensions(0.6F, 1.95F).eyeHeight(1.79F).passengerAttachments(2.0F).vehicleAttachment(-0.7F).maxTrackingRange(8).build());
 
     public static final EntityType<SoulStealerEntity> SOUL_STEALER = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "soul_stealer"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, SoulStealerEntity::new).dimensions(EntityDimensions.fixed(0.6F, 1.99F)).fireImmune().build());
+            EntityType.Builder.create(SoulStealerEntity::new, SpawnGroup.MONSTER).dimensions(0.6F, 1.99F).eyeHeight(1.74F).vehicleAttachment(-0.7F).maxTrackingRange(8).makeFireImmune().build());
 
     public static final EntityType<LavaSlimeEntity> LAVA_SLIME = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier(MobVariants.MOD_ID, "lava_slime"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, LavaSlimeEntity::new).dimensions(EntityDimensions.changing(2.04F, 2.04F)).fireImmune().build());
+            EntityType.Builder.create(LavaSlimeEntity::new, SpawnGroup.MONSTER).makeFireImmune().dimensions(0.52F, 0.52F).eyeHeight(0.325F).spawnBoxScale(4.0F).maxTrackingRange(8).build());
 
     public static void registerModMobs(){
         //cave variants

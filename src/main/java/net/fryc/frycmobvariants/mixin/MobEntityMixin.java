4c150d67e2632ac62f7f18ec4acebf8fac23e07a
Xires87 <frycmod@gmail.com>
2024-11-10 14:08:43 +0100

    all mobs get brief nausea after converting
    (cherry picked from commit 1be70cf9e0938c541fb4eaca50dc654f8541d8b8)

--- src/main/java/net/fryc/frycmobvariants/mixin/MobEntityMixin.java
+++ src/main/java/net/fryc/frycmobvariants/mixin/MobEntityMixin.java
@@ -1,12 +1,14 @@
 package net.fryc.frycmobvariants.mixin;
 
-import net.minecraft.entity.EntityData;
-import net.minecraft.entity.SpawnReason;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.*;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,19 +16,34 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
-abstract class MobEntityMixin {
+abstract class MobEntityMixin extends LivingEntity implements EquipmentHolder, Leashable, Targeter {
 
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     // prevents mobs from converting when spawned with spawn egg or command
     @Inject(method = "initialize(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/LocalDifficulty;" +
             "Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/entity/EntityData;)Lnet/minecraft/entity/EntityData;", at = @At("TAIL"))
     private void setNauseaAfterSpawningWithEgg(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason,
                                                @Nullable EntityData entityData, CallbackInfoReturnable<EntityData> ret) {
-        MobEntity dys = ((MobEntity)(Object)this);
         if(spawnReason == SpawnReason.SPAWN_EGG || spawnReason == SpawnReason.COMMAND){
-            if(dys != null){
-                dys.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10, 0, false, false));
-            }
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10, 0, false, false));
         }
     }
+
+    @WrapOperation(
+            method = "convertTo(Lnet/minecraft/entity/EntityType;Z)Lnet/minecraft/entity/mob/MobEntity;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityType;create(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;")
+    )
+    private Entity setNauseaAfterConverting(EntityType<? extends MobEntity> instance, World world, Operation<Entity> original) {
+        Entity mobEntity = original.call(instance, world);
+        if(mobEntity != null){
+            if(mobEntity instanceof MobEntity mob){
+                mob.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10, 0, false, false));
+            }
+        }
+        return mobEntity;
+    }
+
 }

e7020f6a4235c2fabc3a63bdab3a5abd9042ef1c
Xires87 <lukasz005719@gmail.com>
2025-11-08 11:30:19 +0100

    fixed #11
    (cherry picked from commit e47803774f550e93a0b8bdc59053d2f269983363)

--- src/main/java/net/fryc/frycmobvariants/util/MobConvertingHelper.java
+++ src/main/java/net/fryc/frycmobvariants/util/MobConvertingHelper.java
@@ -10,6 +10,7 @@ import net.fryc.frycmobvariants.mobs.nether.LavaSlimeEntity;
 import net.fryc.frycmobvariants.mobs.nether.SoulStealerEntity;
 import net.fryc.frycmobvariants.tags.ModBiomeTags;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -27,35 +28,21 @@ public class MobConvertingHelper {
     public static void tryToConvertZombie(ZombieEntity zombie, Random random){
         if(zombie.getClass() == ZombieEntity.class){
             int i = (int)zombie.getY();
-            if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.FROZEN_ZOMBIE_SPAWN_BIOMES) && (i > 48 || zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                if(random.nextInt(0, 100) <= MobVariants.config.zombieToFrozenZombieConvertChance) zombie.convertTo(ModMobs.FROZEN_ZOMBIE, true);
+            if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.FROZEN_ZOMBIE_SPAWN_BIOMES) && isAtProperYLevel(i, zombie)){
+                if(shouldConvertToVariant(random, MobVariants.config.zombieToFrozenZombieConvertChance)) zombie.convertTo(ModMobs.FROZEN_ZOMBIE, true);
             }
-            else if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.BLOATED_CORPSE_SPAWN_BIOMES) && (i > 48 || zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                if(random.nextInt(0, 100) <= MobVariants.config.zombieToBloatedCorpseConvertChance) zombie.convertTo(ModMobs.BLOATED_CORPSE, true);
+            else if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.BLOATED_CORPSE_SPAWN_BIOMES) && isAtProperYLevel(i, zombie)){
+                if(shouldConvertToVariant(random, MobVariants.config.zombieToBloatedCorpseConvertChance)) zombie.convertTo(ModMobs.BLOATED_CORPSE, true);
             }
-            else if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.EXPLORER_SPAWN_BIOMES) && (i > 48 || zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                if(random.nextInt(0, 100) <= MobVariants.config.zombieToExplorerConvertChance) zombie.convertTo(ModMobs.EXPLORER, true);
+            else if(zombie.getWorld().getBiome(zombie.getBlockPos()).isIn(ModBiomeTags.EXPLORER_SPAWN_BIOMES) && isAtProperYLevel(i, zombie)){
+                if(shouldConvertToVariant(random, MobVariants.config.zombieToExplorerConvertChance)) zombie.convertTo(ModMobs.EXPLORER, true);
             }
-            else {
-                if(i < MobVariants.config.zombieToForgottenConvertLevelY){
-                    boolean bl = false;
-                    if(MobVariants.config.fixedChanceToConvertZombieUnderSelectedYLevel > -1){
-                        if(random.nextInt(0,100) <= MobVariants.config.fixedChanceToConvertZombieUnderSelectedYLevel){
-                            bl = true;
-                        }
-                    }
-                    else if(random.nextInt(i, 100 + i) < MobVariants.config.zombieToForgottenConvertLevelY){
-                        bl = true;
-                    }
-
-                    if(bl){
-                        zombie.convertTo(ModMobs.FORGOTTEN, true);
-                    }
-                }
+            else if(shouldConvertToCaveVariant(random, i, MobVariants.config.fixedChanceToConvertZombieUnderSelectedYLevel, MobVariants.config.zombieToForgottenConvertLevelY)) {
+                zombie.convertTo(ModMobs.FORGOTTEN, true);
             }
         }
         else if(zombie.getClass() == ZombifiedPiglinEntity.class){
-            if(random.nextInt(0, 100) <= MobVariants.config.zombifiedPiglinConvertChance){
+            if(shouldConvertToVariant(random, MobVariants.config.zombifiedPiglinConvertChance)){
                 zombie.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.GOLDEN_AXE));
                 zombie.convertTo(ModMobs.ZOMBIFIED_PIGLIN_BRUTE, true);
             }
@@ -66,50 +53,36 @@ public class MobConvertingHelper {
         if(skeleton.getClass() == SkeletonEntity.class){
             if(!skeleton.getWorld().getDimension().ultrawarm()){
                 int i = (int)skeleton.getY();
-                if(skeleton.getWorld().getBiome(skeleton.getBlockPos()).isIn(ModBiomeTags.CORSAIR_SPAWN_BIOMES) && (i > 48 || skeleton.getWorld().getBiome(skeleton.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                    if(random.nextInt(0, 100) <= MobVariants.config.skeletonToCorsairConvertChance){
+                if(skeleton.getWorld().getBiome(skeleton.getBlockPos()).isIn(ModBiomeTags.CORSAIR_SPAWN_BIOMES) && isAtProperYLevel(i, skeleton)){
+                    if(shouldConvertToVariant(random, MobVariants.config.skeletonToCorsairConvertChance)){
                         skeleton.equipStack(EquipmentSlot.MAINHAND, CorsairEntity.getCorsairSword());
                         skeleton.convertTo(ModMobs.CORSAIR, true);
                     }
                 }
-                else{
-                    if(i < MobVariants.config.skeletonToUndeadWarriorConvertLevelY){
-                        boolean bl = false;
-                        if(MobVariants.config.fixedChanceToConvertSkeletonUnderSelectedYLevel > -1){
-                            if(random.nextInt(0,100) <= MobVariants.config.fixedChanceToConvertSkeletonUnderSelectedYLevel){
-                                bl = true;
-                            }
-                        }
-                        else if(random.nextInt(i, 100 + i) < MobVariants.config.skeletonToUndeadWarriorConvertLevelY){
-                            bl = true;
-                        }
-
-                        if(bl){
-                            if(skeleton.getMainHandStack().hasEnchantments() && MobVariants.config.undeadWarriorAttributes.alwaysKeepEnchantedBow){
-                                skeleton.convertTo(ModMobs.UNDEAD_WARRIOR, true);
-                            }
-                            else{
-                                skeleton.equipStack(EquipmentSlot.MAINHAND, UndeadWarriorEntity.getUndeadWarriorWeapon());
-                                UndeadWarriorEntity uw = skeleton.convertTo(ModMobs.UNDEAD_WARRIOR, true);
-                                if(uw != null){
-                                    if(!(uw.getMainHandStack().getItem() instanceof RangedWeaponItem)){
-                                        uw.tippedArrowsAmount = -1;
-                                    }
-                                }
+                else if(shouldConvertToCaveVariant(random, i, MobVariants.config.fixedChanceToConvertSkeletonUnderSelectedYLevel, MobVariants.config.skeletonToUndeadWarriorConvertLevelY)){
+                    if(skeleton.getMainHandStack().hasEnchantments() && MobVariants.config.undeadWarriorAttributes.alwaysKeepEnchantedBow){
+                        skeleton.convertTo(ModMobs.UNDEAD_WARRIOR, true);
+                    }
+                    else{
+                        skeleton.equipStack(EquipmentSlot.MAINHAND, UndeadWarriorEntity.getUndeadWarriorWeapon());
+                        UndeadWarriorEntity uw = skeleton.convertTo(ModMobs.UNDEAD_WARRIOR, true);
+                        if(uw != null){
+                            if(!(uw.getMainHandStack().getItem() instanceof RangedWeaponItem)){
+                                uw.tippedArrowsAmount = -1;
                             }
                         }
                     }
                 }
             }
             else {
-                if(random.nextInt(0, 100) <= MobVariants.config.skeletonToSoulStealerConvertChance){
+                if(shouldConvertToVariant(random, MobVariants.config.skeletonToSoulStealerConvertChance)){
                     skeleton.equipStack(EquipmentSlot.MAINHAND, SoulStealerEntity.getSoulsStealerWeapon());
                     skeleton.convertTo(ModMobs.SOUL_STEALER, true);
                 }
             }
         }
         else if(skeleton.getClass() == WitherSkeletonEntity.class){
-            if(random.nextInt(0, 100) <= MobVariants.config.witherSkeletonConvertChance){
+            if(shouldConvertToVariant(random, MobVariants.config.witherSkeletonConvertChance)){
                 ExecutionerEntity.initExecutionerEquipment(skeleton);
                 skeleton.convertTo(ModMobs.EXECUTIONER, true);
             }
@@ -119,25 +92,11 @@ public class MobConvertingHelper {
     public static void tryToConvertSpider(SpiderEntity spider, Random random){
         if(spider.getClass() == SpiderEntity.class){
             int i = (int)spider.getY();
-            if(spider.getWorld().getBiome(spider.getBlockPos()).isIn(ModBiomeTags.TROPICAL_SPIDER_SPAWN_BIOMES) && (i > 48 || spider.getWorld().getBiome(spider.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                if(random.nextInt(0, 100) <= MobVariants.config.spiderToTropicalSpiderConvertChance) spider.convertTo(ModMobs.TROPICAL_SPIDER, false); // ~80% chance to convert in jungle (default)
+            if(spider.getWorld().getBiome(spider.getBlockPos()).isIn(ModBiomeTags.TROPICAL_SPIDER_SPAWN_BIOMES) && isAtProperYLevel(i, spider)){
+                if(shouldConvertToVariant(random, MobVariants.config.spiderToTropicalSpiderConvertChance)) spider.convertTo(ModMobs.TROPICAL_SPIDER, false);
             }
-            else {
-                if(i < MobVariants.config.spiderToArmoredSpiderConvertLevelY){
-                    boolean bl = false;
-                    if(MobVariants.config.fixedChanceToConvertSpiderUnderSelectedYLevel > -1){
-                        if(random.nextInt(0,100) <= MobVariants.config.fixedChanceToConvertSpiderUnderSelectedYLevel){
-                            bl = true;
-                        }
-                    }
-                    else if(random.nextInt(i, 100 + i) < MobVariants.config.spiderToArmoredSpiderConvertLevelY){
-                        bl = true;
-                    }
-
-                    if(bl){
-                        spider.convertTo(ModMobs.ARMORED_SPIDER, false);
-                    }
-                }
+            else if(shouldConvertToCaveVariant(random, i, MobVariants.config.fixedChanceToConvertSpiderUnderSelectedYLevel, MobVariants.config.spiderToArmoredSpiderConvertLevelY)){
+                spider.convertTo(ModMobs.ARMORED_SPIDER, false);
             }
         }
     }
@@ -146,8 +105,8 @@ public class MobConvertingHelper {
         int size = slime.getSize();
         if(slime.getClass() == SlimeEntity.class){
             int i = (int)slime.getY();
-            if(slime.getWorld().getBiome(slime.getBlockPos()).isIn(ModBiomeTags.TOXIC_SLIME_SPAWN_BIOMES) && (i > 48 || slime.getWorld().getBiome(slime.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES))){
-                if(random.nextInt(0, 100) <= MobVariants.config.slimeToToxicSlimeConvertChance){
+            if(slime.getWorld().getBiome(slime.getBlockPos()).isIn(ModBiomeTags.TOXIC_SLIME_SPAWN_BIOMES) && isAtProperYLevel(i, slime)){
+                if(shouldConvertToVariant(random, MobVariants.config.slimeToToxicSlimeConvertChance)){
                     ToxicSlimeEntity toxicSlime = slime.convertTo(ModMobs.TOXIC_SLIME, false);
                     if(toxicSlime != null){
                         toxicSlime.setSize(size, true);
@@ -156,7 +115,7 @@ public class MobConvertingHelper {
             }
         }
         else if(slime.getClass() == MagmaCubeEntity.class){
-            if(random.nextInt(0, 100) <= MobVariants.config.magmaCubeToLavaSlimeConvertChance){
+            if(shouldConvertToVariant(random, MobVariants.config.magmaCubeToLavaSlimeConvertChance)){
                 LavaSlimeEntity lavaSlime = slime.convertTo(ModMobs.LAVA_SLIME, false);
                 if(lavaSlime != null){
                     lavaSlime.setSize(size, true);
@@ -167,7 +126,7 @@ public class MobConvertingHelper {
 
     public static void tryToConvertGhast(GhastEntity ghast, Random random){
         if (ghast.getClass() == GhastEntity.class) {
-            if (random.nextInt(0, 100) <= MobVariants.config.ghastConvertChance) {
+            if (shouldConvertToVariant(random, MobVariants.config.ghastConvertChance)) {
                 ghast.convertTo(ModMobs.NIGHTMARE, false);
             }
         }
@@ -176,37 +135,43 @@ public class MobConvertingHelper {
     public static void tryToConvertCreeper(CreeperEntity creeper, Random random){
         if(creeper.getClass() == CreeperEntity.class){
             int i = (int)creeper.getY();
-            if(i < MobVariants.config.creeperToCaveCreeperConvertLevelY){
-                boolean bl = false;
-                if(MobVariants.config.fixedChanceToConvertCreeperUnderSelectedYLevel > -1){
-                    if(random.nextInt(0,100) <= MobVariants.config.fixedChanceToConvertCreeperUnderSelectedYLevel){
-                        bl = true;
-                    }
-                }
-                else if(random.nextInt(i, 100 + i) < MobVariants.config.creeperToCaveCreeperConvertLevelY){ // ~26% to convert on 0Y level (default)
-                    bl = true;
-                }
-
-                if(bl){
-                    creeper.convertTo(ModMobs.CAVE_CREEPER, false);
-                }
+            if(shouldConvertToCaveVariant(random, i, MobVariants.config.fixedChanceToConvertCreeperUnderSelectedYLevel, MobVariants.config.creeperToCaveCreeperConvertLevelY)){
+                creeper.convertTo(ModMobs.CAVE_CREEPER, false);
             }
         }
     }
 
-    public static void tryToConvertPiglin(AbstractPiglinEntity piglin ,Random random){
+    public static void tryToConvertPiglin(AbstractPiglinEntity piglin, Random random){
         if(piglin.getClass() == PiglinEntity.class){
-            if(random.nextInt(0, 100) <= MobVariants.config.piglinConvertChance){
+            if(shouldConvertToVariant(random, MobVariants.config.piglinConvertChance)){
                 piglin.convertTo(ModMobs.INFECTED_PIGLIN, true);
             }
         }
         else if(piglin.getClass() == PiglinBruteEntity.class){
-            if(random.nextInt(0, 100) <= MobVariants.config.piglinBruteConvertChance){
+            if(shouldConvertToVariant(random, MobVariants.config.piglinBruteConvertChance)){
                 piglin.convertTo(ModMobs.INFECTED_PIGLIN_BRUTE, true);
             }
         }
     }
 
+    public static boolean isAtProperYLevel(int y, LivingEntity mob){
+        return y > 48 || mob.getWorld().getBiome(mob.getBlockPos()).isIn(ModBiomeTags.UNDERGROUND_BIOMES);
+    }
+    
+    public static boolean shouldConvertToVariant(Random random, int chance){
+        return random.nextInt(0, 100) < chance;
+    }
+
+    public static boolean shouldConvertToCaveVariant(Random random, int y, int fixedChance, int convertLevelY) {
+        if(y < convertLevelY){
+            if(fixedChance > -1){
+                return shouldConvertToVariant(random, fixedChance);
+            }
+            else return random.nextInt(y, 100 + y) < convertLevelY;
+        }
+
+        return false;
+    }
 
     public static ItemStack getRandomItemStack(Map<Item, Pair<Float, Float>> map) {
         float chance = ThreadLocalRandom.current().nextFloat();
